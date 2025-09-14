18c19962a83a66db98e262c906e1cec1fd25c934
[firstname lastname] <dhamib610@gmail.com>
2025-09-14 23:18:45 +0545

    change file app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 const corsOptions = {
   origin: [
     "http://localhost:5173", // dev
-    "https://jobportal-eabz.vercel.app" // production frontend
+    "https://https://jobportal-5vx8.vercel.app" // production frontend
   ],
   credentials: true, // allow cookies
 };
