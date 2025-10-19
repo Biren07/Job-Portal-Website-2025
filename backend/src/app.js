abfb3e212f93aa717b73b48b2181eef49b9de0c8
[firstname lastname] <dhamib610@gmail.com>
2025-10-19 21:03:09 +0545

    update file

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: " http://localhost:5173/", 
+  origin: " http://localhost:5173", 
   credentials: true,              
 }));
 
